74d647fff4f017bcc4c05a2ad6aee086ac969df6
Niran N <nirancodes@gmail.com>
2022-01-30 01:34:20 +0530

    bst-30

--- Tree/Binary Search Tree/bst-ll.py
+++ Tree/Binary Search Tree/bst-ll.py
@@ -4,6 +4,24 @@ class BSTNode():
         self.leftChild=None
         self.rightChild=None
 
+      
+def insert(root,value):
+        if root.data == None:
+            root.data = value
+        elif value<=root.data:
+            if root.leftChild is None:
+                root.leftChild ==BSTNode(value) #interesting
+            else:
+                insert(root.leftChild,value)
+        else:
+            if value>root.data:
+                if root.rightChild == None:
+                    root.rightChild == BSTNode(value)
+                else:
+                    insert(root.rightChild,value)
+        return "Inserted successfully"
+   
+
 BST = BSTNode(None)
 
 
