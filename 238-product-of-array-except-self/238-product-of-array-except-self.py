698c46573781ad14ef4c7b9dfc8eb105fca8907f
Niran N <submachinerox@gmail.com>
2022-02-14 00:49:41 +0530

    Time: 972 ms (5.02%), Space: 22.1 MB (29.79%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -0,0 +1,26 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        dt=Counter(nums)
+        op=[]
+        for i in nums:
+            #print(i)
+            dt[i]-=1
+            tp=1
+            #print("dt=",dt)
+            if dt[i]==0:
+                del dt[i]
+            #print("del dt=",dt)
+            for t in dt.keys():
+                print(t,dt[t],tp)
+                tp*=t**dt[t]
+            #print(tp)
+            op.append(tp)
+            #print("hmm",dt,i)
+            if i not in dt: 
+                dt[i]=1
+            else:
+                dt[i]+=1
+            #print("post dt",dt)
+        #print(op)
+        return op
+                
