c1973a0a203e3934d7d62fc89c94707588a2815d
Niran N <submachinerox@gmail.com>
2022-02-14 00:50:32 +0530

    Time: 430 ms (15.32%), Space: 21.2 MB (45.87%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -3,24 +3,16 @@ class Solution:
         dt=Counter(nums)
         op=[]
         for i in nums:
-            #print(i)
             dt[i]-=1
             tp=1
-            #print("dt=",dt)
             if dt[i]==0:
                 del dt[i]
-            #print("del dt=",dt)
             for t in dt.keys():
-                #print(t,dt[t],tp)
                 tp*=t**dt[t]
-            #print(tp)
             op.append(tp)
-            #print("hmm",dt,i)
             if i not in dt: 
                 dt[i]=1
             else:
                 dt[i]+=1
-            #print("post dt",dt)
-        #print(op)
         return op
                 
