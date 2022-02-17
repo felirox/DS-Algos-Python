2418fac1eacb1d91a6395829b7f0fd1fee2e8cc7
Niran N <submachinerox@gmail.com>
2022-02-17 12:31:04 +0530

    Time: 148 ms (27.90%), Space: 14 MB (87.78%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,14 +1,15 @@
 class Solution:
-    def dfs(self,target,candy,path,tp):
-        if target<0:
+    def dfs(self,tar,candy,cp,tp):
+        if tar<0:
             return
-        if target==0:
-            tp.append(path)
+        if tar==0:
+            tp.append(cp)
             return
         for i in range(len(candy)):
-            self.dfs(target-candy[i],candy[i:],path+[candy[i]],tp)
-        
+            self.dfs(tar-candy[i],candy[i:],cp+[candy[i]],tp)
+    
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        ret=[]
-        self.dfs(target,candidates,[],ret)
-        return ret
+        tp=[]
+        self.dfs(target,candidates,[],tp)
+        return tp
+        
