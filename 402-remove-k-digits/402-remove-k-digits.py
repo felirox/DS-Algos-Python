a5aca489a979a1df4d22ad30608ba51297a4bb1e
Niran N <submachinerox@gmail.com>
2022-02-19 00:11:34 +0530

    Time: 46 ms (71.17%), Space: 14.2 MB (67.26%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.py
+++ 402-remove-k-digits/402-remove-k-digits.py
@@ -1,11 +1,10 @@
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        cos=[]
-        for i in range(len(num)):
-            while k and cos and cos[-1]>num[i]:
-                cos.pop()
+        stk=[]
+        for i in num:
+            while k and stk and stk[-1]>i:
+                stk.pop()
                 k-=1
-            cos.append(num[i])
-        print(cos)
-        return ''.join(cos[:-k or None]).lstrip('0')or "0"
+            stk.append(i)
+        return ''.join(stk[:-k or None]).lstrip("0") or "0"
         
