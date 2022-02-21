f26d9cd52a03b8b1b2a99907e916daf3e9170159
Niran N <submachinerox@gmail.com>
2022-02-21 22:44:57 +0530

    Time: 283 ms (25.58%), Space: 15.4 MB (81.81%) - LeetHub

---
+++ 169-majority-element/169-majority-element.py
@@ -0,0 +1,15 @@
+from collections import Counter
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        maxx=-1
+        count=0
+        for i in nums:
+            if count==0:
+                maxx=i
+                count=1
+            else:
+                if maxx==i:
+                    count+=1
+                else:
+                    count-=1
+        return maxx
