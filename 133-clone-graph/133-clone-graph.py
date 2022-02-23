4873747043ef6cf4da7d793b1cac8b871534fe2f
Niran N <submachinerox@gmail.com>
2022-02-23 23:15:24 +0530

    Time: 41 ms (71.05%), Space: 14.4 MB (66.44%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -9,7 +9,6 @@ class Node:
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node: return node
-        
         q, clones = deque([node]), {node.val: Node(node.val, [])}
         while q:
             cur = q.popleft() 
@@ -19,7 +18,5 @@ class Solution:
                 if ngbr.val not in clones:
                     clones[ngbr.val] = Node(ngbr.val, [])
                     q.append(ngbr)
-                    
                 cur_clone.neighbors.append(clones[ngbr.val])
-                
         return clones[node.val]
