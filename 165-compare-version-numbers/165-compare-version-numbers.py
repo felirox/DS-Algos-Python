033ad48c4ae4671d90fb46b99c134cc72909e695
Niran N <submachinerox@gmail.com>
2022-02-25 14:56:17 +0530

    Time: 53 ms (24.79%), Space: 13.9 MB (68.52%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.py
+++ 165-compare-version-numbers/165-compare-version-numbers.py
@@ -8,7 +8,8 @@ class Solution:
             op1=op1+[0]*(len(op2)-len(op1))
         else:
             op2=op2+[0]*(len(op1)-len(op2))
+        # return (op1>op2)-(op2>op1)
 
-        return (op1>op2)-(op2>op1)
+        return 1 if op1>op2 else -1 if op2>op1 else 0
 
             
