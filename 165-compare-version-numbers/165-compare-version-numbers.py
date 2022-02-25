cd1c35c04b5b9f788cc242aa0e5141633754c318
Niran N <submachinerox@gmail.com>
2022-02-25 14:55:00 +0530

    Time: 33 ms (70.95%), Space: 13.8 MB (92.33%) - LeetHub

---
+++ 165-compare-version-numbers/165-compare-version-numbers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+
+        op1=[int(i) for i in version1.split(".")]
+        op2=[int(i) for i in version2.split(".")]
+        
+        if len(op1)<len(op2):
+            op1=op1+[0]*(len(op2)-len(op1))
+        else:
+            op2=op2+[0]*(len(op1)-len(op2))
+
+        return 1 if op1>op2 else -1 if op2>op1 else 0
+
+            
