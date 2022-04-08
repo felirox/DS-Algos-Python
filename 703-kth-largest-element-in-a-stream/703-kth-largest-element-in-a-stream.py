61e15cdae85c43366af487e514c6e85234f885d9
Niran N <submachinerox@gmail.com>
2022-04-08 09:53:48 +0530

    Time: 940 ms (11.92%), Space: 18.5 MB (10.77%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.py
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.py
@@ -1,18 +1,15 @@
-from sortedcontainers import SortedList
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
         self.k=k
-        self.nums=SortedList()
-        self.nums.update(nums)
-        
+        self.nums=nums
+        self.nums.sort()        
 
     def add(self, val: int) -> int:
-        self.nums.add(val)
+        self.nums.append(val)
+        self.nums.sort()
         return self.nums[-self.k]
         
-
-
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
